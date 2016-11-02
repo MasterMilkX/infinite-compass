f800395cc9b0cf8ad3b5805f29a15b181bfcca8a
Milk Charity <mastermilkx@gmail.com>
2016-11-02 14:02:22 -0400

    Version 0.1

---
+++ game.js
@@ -0,0 +1,7 @@
+//set up the canvas
+var canvas = document.createElement("canvas");
+canvas.id = "game";
+var ctx = canvas.getContext("2d");
+canvas.width = 320;
+canvas.height = 320;
+document.body.appendChild(canvas);
